fa848f73d4d067efc949cbbf51f6a79ed4b819d6
Elvis Rivera <therealelvisrivera@gmail.com>
2024-03-25 14:42:17 -0500

    Functional Strikethrough function

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@
 
 //Function to toggle the 'Strikethrough'class
 function toggleStrikethrough(event) {
+      console.log('Item clicked:', event.target); // This should log when you click an item
     //'event.target' is the click element
-    event.target.classList.toggle('Strikethrough');
+    event.target.classList.toggle('strikethrough');
 }
 
 //add click event listeners to all list items
